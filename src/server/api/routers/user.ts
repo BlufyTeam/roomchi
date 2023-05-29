a66e3c06f56dde01efe2807d2b8d4000253b949e
Phonixoop <ali.hassanzadeh78@gmail.com>
2023-05-29 11:38:58 +0330

    zod changes and create input data

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -9,25 +9,27 @@ export const userRouter = createTRPCRouter({
   getUser: protectedProcedure.query(({ ctx }) => {
     return ctx.session.user;
   }),
-  createUser: protectedProcedure.input(z.object({
-     name: z.string() ,
-     email:z.string(),
-     username:z.string().min(3,'یوزرنیم نمیتواند کمتر از 3 حرف باشد'),
-     password:z.string().min(6,'پسورد نمیتواند کمتر از 6 حرف باشد.'),
-     description:z.string(),
-     role:z.enum(['ADMIN',"USER"]),
-    })).mutation(async({input, ctx }) => {
-      
-    return await ctx.prisma.user.create( {
-        name: input.name,
-        email: input.email,
-        username:input.username,
-        password:input.password,
-        description:input.description,
-        role:input.role
-
-      },
+  createUser: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        email: z.string().email(),
+        username: z.string().min(3, "یوزرنیم نمیتواند کمتر از 3 حرف باشد"),
+        password: z.string().min(6, "پسورد نمیتواند کمتر از 6 حرف باشد."),
+        description: z.string(),
+        role: z.enum(["ADMIN", "USER"]),
+      })
     )
-  }),
-  
+    .mutation(async ({ input, ctx }) => {
+      return await ctx.prisma.user.create({
+        data: {
+          name: input.name,
+          email: input.email,
+          username: input.username,
+          password: input.password,
+          description: input.description,
+          role: input.role,
+        },
+      });
+    }),
 });
