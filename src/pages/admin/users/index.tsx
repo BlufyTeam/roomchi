618bc147007536476c951e1dca71a2847c667ba6
Phonixoop <ali.hassanzadeh78@gmail.com>
2023-05-30 09:42:18 +0330

    responsive on 2xl

--- src/pages/admin/users/index.tsx
+++ src/pages/admin/users/index.tsx
@@ -14,14 +14,14 @@ export default function RoomsPage() {
   return (
     <AdminMainLayout>
       <ContainerBottomBorder className="h-full items-start bg-accent/5 ">
-        <Container className="flex flex-col-reverse items-stretch gap-10 py-10  md:flex-row ">
-          <div className="sticky top-5 h-fit rounded-lg border border-accent/30 bg-secondary p-5 md:w-3/12">
+        <Container className="flex flex-col-reverse items-stretch gap-10 py-10  2xl:flex-row ">
+          <div className="sticky top-5 h-fit rounded-lg border border-accent/30 bg-secondary p-5 2xl:w-3/12">
             <UserForm
               user={selectedRowUser}
               onClearUser={() => setSelectedRowUser(undefined)}
             />
           </div>
-          <div className="h-full max-h-[77%] overflow-hidden rounded-lg  border border-accent/30 bg-secondary p-5 md:w-9/12">
+          <div className="h-full max-h-[77%] overflow-hidden rounded-lg  border border-accent/30 bg-secondary p-5 2xl:w-9/12">
             <UsersList
               onRowClick={(user: User) => {
                 setSelectedRowUser(user);
