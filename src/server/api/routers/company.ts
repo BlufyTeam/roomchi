8610ec988aa3b64fae8d16c1c98e80b59efd9a5d
Sepehrni <sepehrni@yahoo.com>
2023-05-31 10:06:36 +0330

    get and update company API added

---
+++ src/server/api/routers/company.ts
@@ -0,0 +1,40 @@
+import { z } from "zod";
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+import {
+  createUserSchema,
+  updateUserSchema,
+  userIdSchema,
+} from "~/server/validations/user.validation";
+import {companyIdSchema, updateCompanySchema} from '~/server/validations/company.validation'
+
+export const companyRouter = createTRPCRouter({
+  getCompanyById: protectedProcedure.input(companyIdSchema).query(({ ctx ,input}) => {
+    return ctx.prisma.company.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+  }),
+
+ 
+  updateCompany: protectedProcedure
+    .input(updateCompanySchema)
+    .mutation(async ({ input, ctx }) => {
+      return await ctx.prisma.user.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          name: input.name,
+          description: input.description,
+
+        },
+      });
+    }),
+  
+
+});
---
+++ src/server/validations/company.validation.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+import { Base64 } from 'js-base64';
+
+export const updateCompanySchema = z.object({
+    id:z.string({ required_error: "این فیلد اجباری است" }),
+  name: z
+    .string({ required_error: "این فیلد اجباری است" })
+    .min(3, "حداقل باید 3 کاراکتر باشد"),
+  description: z.string().optional(),
+  logo_url:z.string().optional().refine(Base64.isValid,{
+    message: "error:its not base64",
+  })
+
+});
+
+
+
+
+export const companyIdSchema = z.object({
+  id: z.string({ required_error: "این فیلد اجباری است" }),
+});
+
+
