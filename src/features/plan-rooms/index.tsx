052cd14eeae1a918d736306c168a3326ff1dd2b9
Phonixoop <ali.hassanzadeh78@gmail.com>
2023-06-10 13:53:04 +0330

    loading added to PlanRooms

--- src/features/plan-rooms/index.tsx
+++ src/features/plan-rooms/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import PickTimeView from "~/features/pick-time-view";
 import PlanListWithRoom from "~/features/plan-list-with-room";
 import RoomsList from "~/features/rooms-list";
+import { RoomsListSkeleton } from "~/features/rooms-list/loading";
 import { api } from "~/utils/api";
 type Props = {
   date: Moment;
@@ -11,7 +12,7 @@ export default function PlanRooms({ date }: Props) {
   const getPlans = api.plan.getPlansByDate.useQuery({
     date: date.toDate(),
   });
-  if (getPlans.isLoading) return <>loaidng</>;
+  if (getPlans.isLoading) return <RoomsListSkeleton />;
 
   const canPickTime = date.isSameOrAfter(moment(), "jDay");
   return (
--- src/features/rooms-list/index.tsx
+++ src/features/rooms-list/index.tsx
@@ -21,6 +21,7 @@ import {
 import Image from "next/image";
 import React from "react";
 import { useRoom } from "~/context/room.context";
+import { RoomsListSkeleton } from "~/features/rooms-list/loading";
 import { RoomStatus } from "~/types";
 import ProjectorIcon from "~/ui/icons/projector";
 import { api } from "~/utils/api";
@@ -130,88 +131,5 @@ function RoomItem({
         <span>{room.price} تومان</span>
       </div>
     </>
-  );
-}
-
-function RoomsListSkeleton() {
-  return (
-    <div className=" grid gap-5 md:grid-cols-3 ">
-      {[...Array(32).keys()].map((i) => {
-        return (
-          <>
-            <div
-              key={i}
-              className="flex h-52 animate-pulse  flex-col   rounded-xl bg-primary/20 opacity-30"
-              style={{
-                animationDelay: `${i * 5}`,
-                animationDuration: "1s",
-              }}
-            >
-              <div className="flex">
-                <div className="mx-auto w-full max-w-sm rounded-md  p-4">
-                  <div className="flex animate-pulse gap-5">
-                    <div className="h-10 w-10 rounded-full bg-secondary"></div>
-                    <div className="flex-1 space-y-6 py-1">
-                      <div className="h-2 rounded bg-secondary"></div>
-                      <div className="space-y-3">
-                        <div className="grid grid-cols-3 gap-4">
-                          <div className="col-span-1 h-2 rounded bg-secondary"></div>
-                          <div className="col-span-2 h-2 rounded bg-secondary"></div>
-                        </div>
-                        <div className="h-2 rounded bg-secondary"></div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="mx-auto w-full max-w-sm rounded-md  p-4">
-                  <div className="flex animate-pulse gap-5">
-                    <div className="flex-1 space-y-6 py-1">
-                      <div className="h-2 rounded "></div>
-                      <div className="space-y-3">
-                        <div className="grid grid-cols-3 gap-4">
-                          <div className="col-span-1 h-2 rounded bg-secondary"></div>
-                          <div className="col-span-2 h-2 rounded bg-secondary"></div>
-                        </div>
-                        <div className="h-2 rounded bg-secondary"></div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="flex">
-                <div className="mx-auto w-full max-w-sm rounded-md  p-4">
-                  <div className="flex animate-pulse gap-5">
-                    <div className="flex-1 space-y-6 py-1">
-                      <div className="h-2 rounded bg-secondary"></div>
-                      <div className="space-y-3">
-                        <div className="grid grid-cols-3 gap-4">
-                          <div className="col-span-2 h-2 rounded bg-secondary"></div>
-                          <div className="col-span-1 h-2 rounded bg-secondary"></div>
-                        </div>
-                        <div className="h-2 rounded bg-secondary"></div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="mx-auto w-full max-w-sm rounded-md  p-4">
-                  <div className="flex animate-pulse gap-5">
-                    <div className="flex-1 space-y-6 py-1">
-                      <div className="h-2 rounded bg-secondary"></div>
-                      <div className="space-y-3">
-                        <div className="grid grid-cols-3 gap-4">
-                          <div className="col-span-2 h-2 rounded bg-secondary"></div>
-                          <div className="col-span-1 h-2 rounded bg-secondary"></div>
-                        </div>
-                        <div className="h-2 rounded bg-secondary"></div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </>
-        );
-      })}
-    </div>
   );
 }
