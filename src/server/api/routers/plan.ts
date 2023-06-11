926fe90ffe6c6a0a17e7a78ad9fdd14ed0b9144a
Phonixoop <ali.hassanzadeh78@gmail.com>
2023-06-11 19:04:52 +0330

    let changed to const 🤬😡

--- src/server/api/routers/plan.ts
+++ src/server/api/routers/plan.ts
@@ -15,7 +15,7 @@ import {
 import { RoomStatus } from "~/types";
 
 export function $exists<T>(ts: T[]): boolean {
-  return ts.length > 0
+  return ts.length > 0;
 }
 export const planRouter = createTRPCRouter({
   getPlansByRoomId: protectedProcedure
@@ -112,38 +112,40 @@ export const planRouter = createTRPCRouter({
   createPlan: protectedProcedure
     .input(createPlanSchema)
     .mutation(async ({ input, ctx }) => {
-   let isExists=await  ctx.prisma.plan.findMany({
-    where:{ OR: [
-      {
-        start_datetime: { lte: input?.end_datetime },
-        end_datetime: { gte: input?.start_datetime },
-      },
-      {
-        start_datetime: { gte: input?.start_datetime },
-        end_datetime: { lte: input?.end_datetime },
-      },
-    ]}
-    
-   }).then($exists)
-   
- 
-  if(isExists){
-return {
-  code:409,
-  error:'جلسه ای در این زمان از قبل وجود دارد.'}
-  }else{
-    return await ctx.prisma.plan.create({
-      data: {
-        title: input.title,
-        userId: ctx.session.user.id,
-        roomId: input.roomId,
-        start_datetime: input.start_datetime,
-        description: input.description,
-        end_datetime: input.end_datetime,
-      },
-    });
-  }
-      
+      const isExists = await ctx.prisma.plan
+        .findMany({
+          where: {
+            OR: [
+              {
+                start_datetime: { lte: input?.end_datetime },
+                end_datetime: { gte: input?.start_datetime },
+              },
+              {
+                start_datetime: { gte: input?.start_datetime },
+                end_datetime: { lte: input?.end_datetime },
+              },
+            ],
+          },
+        })
+        .then($exists);
+
+      if (isExists) {
+        return {
+          code: 409,
+          error: "جلسه ای در این زمان از قبل وجود دارد.",
+        };
+      } else {
+        return await ctx.prisma.plan.create({
+          data: {
+            title: input.title,
+            userId: ctx.session.user.id,
+            roomId: input.roomId,
+            start_datetime: input.start_datetime,
+            description: input.description,
+            end_datetime: input.end_datetime,
+          },
+        });
+      }
     }),
   updateUser: protectedProcedure
     .input(updatePlanSchema)
