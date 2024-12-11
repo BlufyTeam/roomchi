8f1dd1575051c2adb17c29b45245dd1686dc7507
Phonixoop <ali.hassanzadeh78@gmail.com>
2024-12-11 11:08:31 +0330

    next config update

--- next.config.mjs
+++ next.config.mjs
@@ -6,17 +6,26 @@ await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
 
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    return config;
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  compress: false,
+  output: "standalone",
+  images: {
+    minimumCacheTTL: 31536000,
   },
 };
+
 export default config;
